3d53a37f45ad9c488f48c533f66218b783427bbf
ARMAAN uni <asc36@sfu.ca>
2024-10-29 17:55:34 -0700

    added button functionality to scroll up

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+backToTopBtn.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
